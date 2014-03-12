1f5e4193b035b08544dd3c895908a3d9068008ad
Chris LaRose <cjlarose@gmail.com>
2014-03-12 21:33:23 +0000

    Implemented column operations

--- matrix.cpp
+++ matrix.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <iomanip>
+#include "quicksort.h"
 #include "matrix.h"
 
 Matrix::Matrix(long m, long n)
@@ -34,4 +35,38 @@ std::istream& operator>>(std::istream &in, const Matrix& matrix) {
         for (int i = 0; i < matrix.rows(); ++i)
             in >> matrix(i, j);
     return in;
+}
+
+/*
+ * Sorting a column j in our column-order matrix is actually just
+ * sorting the ith row.
+ */
+void Matrix::sort_column(long j) {
+    quicksort(&elements[j], 0, m - 1);
+}
+
+void Matrix::transpose_column(Matrix& dest, long j) {
+    long start_row = j * (m / n);
+    for (long i = 0; i < m; ++i)
+        dest(start_row + i / n, i % n) = (i, j);
+}
+
+void Matrix::reverse_transpose_column(Matrix& dest, long j) {
+    for (long i = 0; i < m; i++)
+        dest( ((n * i) + j) % m, i / (m/n) ) = (i, j);
+}
+
+/*
+ * Instead of actually moving elements around and then sorting by column,
+ * We can just sort with an offset, which is facilitated by the fact that
+ * our matrix is stored in column-order form. Sorting the 0th column here
+ * actually means to sort the first m - (n / 2) elements, then sorting the
+ * last n / 2 elements.
+ */
+void Matrix::sort_column_shift(long j, long shift) {
+    if (j == 0) {
+        quicksort(&elements[0], 0, m - shift - 1);
+        quicksort(&elements[n - 1], m - shift, m - 1);
+    } else
+        quicksort(&elements[j] - shift, 0, m - 1);
 }
--- matrix.h
+++ matrix.h
@@ -8,6 +8,10 @@ class Matrix {
         long* elements;
         long m;
         long n;
+        void sort_column(long j);
+        void sort_column_shift(long j, long shift);
+        void transpose_column(Matrix& dest, long j);
+        void reverse_transpose_column(Matrix& dest, long j);
 };
 
 std::ostream& operator<<(std::ostream &strm, const Matrix& matrix);
