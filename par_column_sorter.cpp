0e5922d4b72ed3111cb1d3906a70cc48d5a5670b
Chris LaRose <cjlarose@gmail.com>
2014-03-13 01:51:25 -0700

    Removed commented code

--- par_column_sorter.cpp
+++ par_column_sorter.cpp
@@ -9,12 +9,6 @@ ParColumnSorter::ParColumnSorter(long r, long s, int p)
         p = s;
 }
 
-/*
-void ParColumnSorter::worker(worker_message_t* message) {
-    for (long j = message->start; j < message->end; ++j)
-        message->left.sort_column(j);
-}
-*/
 void ParColumnSorter::worker(Matrix& left, Matrix& right, long start, 
     long end) {
     for (long j = start; j < end; ++j)
@@ -22,10 +16,6 @@ void ParColumnSorter::worker(Matrix& left, Matrix& right, long start,
 }
 
 void ParColumnSorter::sort() {
-    /*
-    worker_message_t message = {left, right, 0, s};
-    std::thread sorter(worker, &message);
-    */
     std::thread sorter(&ParColumnSorter::worker, std::ref(left), std::ref(right), 0, s);
 
     sorter.join();
--- par_column_sorter.h
+++ par_column_sorter.h
@@ -4,21 +4,11 @@
 #include "column_sorter.h"
 #include "matrix.h"
 
-/*
-typedef struct {
-    Matrix& left;
-    Matrix& right;
-    long start;
-    long end;
-} worker_message_t;
-*/
-
 class ParColumnSorter: public ColumnSorter {
     public:
         ParColumnSorter(long r, long s, int p);
         void sort();
     private:
-        //static void worker(worker_message_t* msg);
         static void worker(Matrix& left, Matrix& right, long start, long end);
         int p;
 };
