c558700e9cc9cb5f27436afcb9cb003443e1acfa
Chris LaRose <cjlarose@gmail.com>
2014-03-13 01:24:32 -0700

    Worker thread not working

--- par_column_sorter.cpp
+++ par_column_sorter.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <thread>
 #include "par_column_sorter.h"
 #include "matrix.h"
 
@@ -8,5 +9,23 @@ ParColumnSorter::ParColumnSorter(long r, long s, int p)
         p = s;
 }
 
+/*
+void ParColumnSorter::worker(worker_message_t* message) {
+    for (long j = message->start; j < message->end; ++j)
+        message->left.sort_column(j);
+}
+*/
+void worker(Matrix& left, Matrix& right, long start, long end) {
+    for (long j = start; j < end; ++j)
+        left.sort_column(j);
+}
+
 void ParColumnSorter::sort() {
+    /*
+    worker_message_t message = {left, right, 0, s};
+    std::thread sorter(worker, &message);
+    */
+    std::thread sorter(worker, left, right, 0, s);
+
+    sorter.join();
 }
